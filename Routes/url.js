30fd6904ee2a9815c9962770062b60da946358ee
zar7an <akashkishore068@gmail.com>
2023-02-14 19:59:50 +0530

    changes

--- Routes/url.js
+++ Routes/url.js
@@ -3,13 +3,14 @@ const router = express.Router()
 const shortUniqueId = require('short-unique-id')
 const uriModel = require('../models/modelUrl')
 
+const baseUrl = 'spotless-suit-foal.cyclic.app'
 
 router.post('/', async (req, res) => {
   try{
     const {longUrl} = req.body
     const cheack = await uriModel.findOne({longUrl: longUrl})
     if(cheack != null) {
-      return res.status(200).json({shortUrl: `${req.baseUrl}/${cheack.id}`})
+      return res.status(200).json({shortUrl: `${baseUrl}/${cheack.id}`})
     } 
     const uid = new shortUniqueId()
     const id = uid(6)
@@ -18,7 +19,7 @@ router.post('/', async (req, res) => {
       longUrl: longUrl
     })
     await doc.save()
-    return res.status(200).json({shortUrl: `${req.baseUrl}/${id}`})
+    return res.status(200).json({shortUrl: `${baseUrl}/${id}`})
 
   }catch(e) {
     console.error(e)
